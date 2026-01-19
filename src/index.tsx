488f2d6f599842db3f4b969becc74913cae17fe7
egorstukov <elopatov74@outlook.com>
2026-01-19 23:00:50 +0300

    task 2

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const PLACES_COUNT = 312;
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App placesCount={PLACES_COUNT} />
   </React.StrictMode>
 );
