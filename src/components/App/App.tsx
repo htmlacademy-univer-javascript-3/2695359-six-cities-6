ccc3ae02641257874964d4ad94d7ff8094e7bc6c
egorstukov <elopatov74@outlook.com>
2026-01-19 23:11:44 +0300

    task 3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,10 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../MainPage/MainPage';
+import LoginPage from '../LoginPage/LoginPage';
+import FavoritesPage from '../FavoritesPage/FavoritesPage';
+import OfferPage from '../OfferPage/OfferPage';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
 
 type AppProps = {
   placesCount: number;
@@ -6,7 +12,22 @@ type AppProps = {
 
 function App({ placesCount }: AppProps): JSX.Element {
   return (
-    <MainPage placesCount={placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage placesCount={placesCount} />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="/favorites"
+          element={
+            <PrivateRoute isAuthorized={false}>
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/offer/:id" element={<OfferPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom';
+
+function NotFoundPage(): JSX.Element {
+  return (
+    <div className="page page--gray page--main">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <Link className="header__logo-link" to="/">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--index page__main--index-empty">
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="cities">
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">404 Not Found</b>
+                <p className="cities__status-description">
+                  The page you are looking for does not exist.
+                </p>
+                <Link to="/" className="button">
+                  Go to main page
+                </Link>
+              </div>
+            </section>
+            <div className="cities__right-section"></div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default NotFoundPage;
